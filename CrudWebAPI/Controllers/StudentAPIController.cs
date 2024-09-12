7efbaf85e3073c34fba3e7c3eee04426027905d9
Nikhil Mahajan <mahajan12647@gmail.com>
2024-09-12 18:15:55 +0530

    Add project files.

---
+++ CrudWebAPI/Controllers/StudentAPIController.cs
@@ -0,0 +1,78 @@
+﻿using CrudWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly MyDbContext context;
+
+        public StudentAPIController(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await context.Students.ToListAsync();
+
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var data = await context.Students.FindAsync(id);
+
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return data;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<ActionResult<Student>> UpdateStudent(int id, Student std)
+        {
+            if (id != std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return Ok(std);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<Student>> DeleteStudent(int id, Student std)
+        {
+            if (id != std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+
+            return Ok(std);
+        }
+    }
+}
---
+++ CrudWebAPI/Models/MyDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudWebAPI.Models;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)=> optionsBuilder.UseSqlServer();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Table__3214EC07B988F20D");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(100)
+                .IsFixedLength();
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(10)
+                .IsFixedLength();
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
